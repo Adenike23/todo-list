e2b761a602c4442433469171ba3d9344af46eb71
Adenike23 <adeofeadenike@gmail.com>
2023-07-29 15:23:47 +0100

    updated local storage

--- js/main.js
+++ js/main.js
@@ -5,7 +5,7 @@ const input = document.querySelector('input');
 const error = document.querySelector('.error');
 const button = document.querySelector('button');
 const ul = document.querySelector('ul')
-
+let todoItemsArray = [] || JSON.parse(localStorage.getItem('todoItems'))
 button.addEventListener('click', function() {
     if (input.value.length <= 0) {
         error.style.display = 'block';
@@ -14,8 +14,17 @@ button.addEventListener('click', function() {
         }, 3000);
     } else {
         let usersInput = input.value
+        todoItemsArray.push(usersInput)
+        localStorage.setItem('todoItems', JSON.stringify(todoItemsArray))
         ul.innerHTML += `<li>${usersInput}</li>`
         input.value = ''
     } 
 })
 
+
+let li = document.createElement('li')
+todoItemsArray.map(todo => (
+    // `<li>${todo}</li>`
+    li.textContent = todo
+))
+ul.appendChild(li)
